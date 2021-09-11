ad3230e0eaada02be111b15d1e51a5c01eb29c02
LegalPirates2020 <legalpirates2020@gmail.com>
2021-09-11 20:30:08 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router';
 import './App.css';
 
+import Home from './pages/Home/Home.page';
+import Login from './pages/Login/Login.page';
+import SignUp from './pages/SignUp/SignUp.page';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/signup' component={SignUp} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
